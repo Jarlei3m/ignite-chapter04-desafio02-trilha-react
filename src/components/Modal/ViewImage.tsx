d72073c095f3cda07ffd29abdd3f033ef1dd435c
Jarlei <jarlei_rodrigues@outlook.com>
2021-06-30 09:41:08 -0300

    chore: Added modal

--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -20,4 +20,32 @@ export function ModalViewImage({
   imgUrl,
 }: ModalViewImageProps): JSX.Element {
   // TODO MODAL WITH IMAGE AND EXTERNAL LINK
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent maxW="900px" maxH="600px" p={0} bgColor="#1b1a18">
+        <ModalBody padding={0}>
+          <Image margin="0 auto" maxH="600px" src={imgUrl} />
+        </ModalBody>
+        <ModalFooter
+          borderBottomRadius={6}
+          bgColor="#1f1f1f"
+          flex="1"
+          height="100"
+          justifyContent="flex-start"
+        >
+          <Link
+            fontSize="14px"
+            lineHeight="16px"
+            color="#f3f2f2"
+            href={imgUrl}
+            isExternal
+          >
+            Abrir original
+          </Link>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
 }
